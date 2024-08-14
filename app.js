fa57cf63c85b2423755a4966f43c5788b0c898e2
leeeeejieun <arjleun19@gmail.com>
2024-08-14 23:07:17 +0900

    로그인 뷰(view) 최적화

--- app.js
+++ app.js
@@ -7,45 +7,22 @@ const app = express();
     콜백 함수는 req(요청 객체)와 res(응답 객체)를 매개변수로 받아 요청을 처리하고, 응답을 보낸다.
 */
 
+// 템플릿 파일(뷰 파일)들이 위치한 디렉터리 설정 
+app.set("views", "./views");
 /* 
-   ! 서버 코드 내부에 HTML을 직접 작성하여 클라이언트에게 응답으로 보내는 하드코딩 방식
-    단점은 다음과 같다.
-    1. 유지보수 어려움 : HTML 구조&내용 변경할 때마다 서버 코드를 수정해야 한다.
-    2. 재사용성 부족 : 다른 경로에서 동일한 HTML을 사용하려면 중복된 코드를 작성해야 한다.
-    3. 확장성 문제: 코드가 복잡해지거나 여러 페이지가 필요한 경우 관리하기가 어렵다.
+    템플릿 엔진으로 EJS 설정
+    뷰 파일의 확장자가 .ejs인 경우, EJS 엔진을 사용해 HTML 템플릿을 렌더링하도록 지정
+    ! npm install ejs -S 모듈 설치 필요
 */
-app.get("/login",(req, res) =>{
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <input type="text" placeholder="아이디"><br>
-            <input type="text" placeholder="비밀번호"><br>
-            <button>로그인</button>
-        </body>
-        </html>
-    `);
+app.set("view engine", "ejs");
+
+// "/" 경로로 GET 요청이 들어오면 index.ejs 템플릿을 렌더링
+app.get("/", (req, res)=>{
+    res.render("home/index");
 });
 
-app.get("/", (req,res)=>{
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="ko">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            루트 페이지 입니다.
-        </body>
-        </html>
-    `);
+app.get("/login",(req, res) =>{
+    res.render("home/login");
 });
 
 // 3000번 포트에서 HTTP 서버 실행
