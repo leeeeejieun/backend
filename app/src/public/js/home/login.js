d28853a10a99a1b8650574c4011a49eefd857a51
leeeeejieun <arjleun19@gmail.com>
2024-08-21 18:18:08 +0900

    로그인 인증 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,6 @@ async function login() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    }).then((res)=> res.json())   // fetch는 response 객체를 반환 , res.json()은 이 응답을 JSON으로 파싱하는 promise를 반환
+      .then((data)=> console.log(data));       // JSON 객체로 파싱된 응답 데이터
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { use } = require(".");
+
 /* 
     Controller : MVC(Model-View-Controller) 패턴의 구성 요소 중 하나로,
     사용자로부터 받은 요청을 처리한 후, 그에 대한 응답을 반환하는 역할을 한다.
@@ -16,11 +18,34 @@ const output = {
     },
 };
 
+// 사용자 데이터 저장 객체
+const users = {
+    id: ["이지은","남예준","채봉구"],
+    password : ["1109","1234","5678"],
+};
+
+// 로그인 인증 기능 구현
 const process ={
     login : (req, res) =>{
-        console.log(req.body);
-    }
-}
+        const {id, password} = req.body;            // front에서 받은 요청 객체의 id, password를 구조 분해 할당하여 저장
+
+        // 로그인이 성공한 경우
+        if(users.id.includes(id)){                  // users에서 해당 id를 포함하고 있는지 확인
+            const idx = users.id.indexOf(id);       // 포함한다면 id의 index 값을 가져옴
+            if(users.password[idx] === password ){  // 해당 index값에 있는 password와 입력한 password가 일치한지 확인
+                return res.json({                   // json 형식으로 로그인 결과(성공)를 front에게 전달
+                    success : true,
+                });
+            }
+        }
+
+        // 로그인이 실패한 경우
+        return res.json({                   
+            success : false,
+            msg : "로그인에 실패하셨습습니다.",
+        });
+    },
+};
 /* 
     Object 형식으로 모듈을 export 
     Object는 {key : value}로 이루어져있다.
